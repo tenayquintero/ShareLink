d1f0ccf4811b4eff3e723e66ab8a512e062d0241
Yaneth Quintero <quinteroftv@hotmail.com>
2022-07-30 14:16:03 +0100

    Tablas creadas, admin creado, esquema relacional creado

--- db/initDB.js
+++ db/initDB.js
@@ -2,39 +2,80 @@
 require('dotenv').config();
 
 const getDB=require('./db')
-let connection;
 
-//const main = async()=>{
-    async function main() {   
+const main = async()=>{
+ 
+  let connection;
 try{
-    console.log(process.env.USER);
-    console.log(process.env.PASSWORD_USER);
- connection = await getDB();
 
+  //Crear la conexión
+ connection = await getDB();
+ 
+//Eliminar tablas si existen
+  await connection.query(`DROP TABLE IF EXISTS votes_links`)
+  await connection.query(`DROP TABLE IF EXISTS links`)
+  await connection.query(`DROP TABLE IF EXISTS users`)
+
+  //Crear las tablas
+ await  connection.query(`
+ 
+     CREATE TABLE users(
+     id_user INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+     date DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL,
+     name VARCHAR(50) NOT NULL,
+     email VARCHAR(100) UNIQUE NOT NULL,
+     password VARCHAR(150) NOT NULL,
+     perfil VARCHAR(150),
+     role ENUM("admin", "normal"),
+     active BOOLEAN DEFAULT false,
+     registation_code VARCHAR(150),
+     last_up_ps DATETIME,
+     deleted BOOLEAN DEFAULT false
+     );
+ `);
  await connection.query(`
-    CREATE TABLE users(
-        id_user INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
-        date DATETIME NOT NULL,
-        name VARCHAR(50) NOT NULL,
-        email VARCHAR(100) UNIQUE NOT NULL,
-        password VARCHAR(150) NOT NULL,
-        perfil VARCHAR(150),
-        role ENUM("admin", "normal"),
-        active BOOLEAN DEFAULT false,
-        registation_code VARCHAR(150),
-        last_up_ps DATETIME,
-        deleted BOOLEAN DEFAULT false
-        )
+    CREATE TABLE links(
+    id_link INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    date DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL, 
+    url VARCHAR(250) NOT NULL,
+    tittle VARCHAR(100) NOT NULL,
+    description VARCHAR(200),
+    id_user INT NOT NULL,
+    FOREIGN KEY (id_user) REFERENCES users(id_user)
+   );
  `);
 
-}catch(error){
-    console.error(error.message);
+ await connection.query(`
+   CREATE TABLE votes_links(
+   id_votes INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+   date DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL,
+   vote TINYINT,
+   CONSTRAINT votes_link CHECK (vote IN (1,2,3,4,5)),
+   id_user INT NOT NULL,
+   FOREIGN KEY (id_user) REFERENCES users(id_user),
+   id_link INT NOT NULL,
+   FOREIGN KEY (id_link) REFERENCES links(id_link)
+)
+ `);
 
-throw new error('Conection not possible')
+ //Creación de usuario admin
+ //Chicos la base de datos tiene por default current_timestamp por lo tanto se genera sola.
+ await connection.query(`
+   INSERT INTO users(name,email,password,role,active)
+   VALUES("Gregorio","gregorio@mail.com",SHA2("${process.env.PASSWORD_USER_ADMIN}",512),"admin",true)
+ `)
+
+}catch(error){
+
+  console.log(error)
+throw new error('Conection is not possible')
 
 }finally{
- if(connection) connection.release();
+
+  //Solatar la conexión
+ if(connection) {connection.release()
  process.exit();
+}
 }
 }
 main();
