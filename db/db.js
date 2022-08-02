1f07a77dee21ebc36200855b84a18b35b049e467
Yaneth Quintero <quinteroftv@hotmail.com>
2022-08-02 10:05:22 +0100

    Cambio de nombre a las propiedades .env referidas a la base de datos

--- db/db.js
+++ db/db.js
@@ -1,7 +1,7 @@
 "use strict"
 const mysql= require('mysql2/promise');
 
-const { HOST,USER,PASSWORD_USER,DATABASE }=process.env;
+const { MYSQL_HOST, MYSQL_USER, PASSWORD_USER,MYSQL_DATABASE }=process.env;
 
 let pool;
 
@@ -9,10 +9,10 @@ const getDB=async()=>{
 if(!pool){
      pool= mysql.createPool({
        connectionLimit:10,
-        user:USER,
-        host:HOST,
+          user:MYSQL_USER,
+          host:MYSQL_HOST,
         password:PASSWORD_USER,
-        database:DATABASE,
+          database:MYSQL_DATABASE,
         timezone:"Z"
      });
      return await pool.getConnection();
