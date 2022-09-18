fe30aa52f3dfa43e42753fb6bc800582ff01d790
Yaneth Quintero <quinteroftv@hotmail.com>
2022-09-18 13:21:46 +0100

    css Auth-forms

--- front-sharelink/src/App.js
+++ front-sharelink/src/App.js
@@ -8,7 +8,7 @@ import './App.css';
 
 function App() {
   return (
-    <main className="App">
+    <main className="App" >
       <Header />
       <Routes>
         <Route path='/' element={<Home />} />
--- front-sharelink/src/Pages/SignUp.js
+++ front-sharelink/src/Pages/SignUp.js
@@ -1,50 +1,48 @@
 import { useState } from "react";
-
+import { Link } from "react-router-dom";
+import './SignUp.css'
 
 const SignUp = () => {
 
-    const [email,setEmail] = useState('');
+    const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
     const [status, setStatus] = useState()
-    const [message, setMessage] = useState()   
-    
-    
-    
+    const [message, setMessage] = useState()
 
-     const handle = async (e) =>{
+    const handle = async (e) => {
         e.preventDefault()
         const res = await fetch('http://127.0.0.1:3000/users', {
             method: 'POST',
-            headers: {'Content-Type': 'application/json'},
+            headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify({
                 email,
                 password
             })
         })
         const resData = await res.json()
-        if(resData.status === 'ok'){
+        if (resData.status === 'ok') {
             setStatus('ok')
             setMessage(resData.message)
-        }else{
+        } else {
             setStatus('error')
             setMessage(resData.message)
-          
-           console.log(resData)
+
+            console.log(resData)
         }
-        
-     }
-     
+
+    }
 
     return (
-        <section>
-            <h2>SignUp</h2>
+        <section className='pages-auth'>
+            <h2><Link to='/'>X</Link></h2>
             <form onSubmit={handle}>
+                <h2>SignUp</h2>
                 <label name='email'>
-                    <input 
-                    id="email"
-                    value={email}
-                    onChange={(e)=>setEmail(e.target.value)}
-                    type='email'
+                    <input
+                        id="email"
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                        type='email'
                     />
                 </label>
                 <label name='password'>
