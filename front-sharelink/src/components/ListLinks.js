c520df2e8d968a42019775aa8f857922ce46ba30
Yaneth Quintero <quinteroftv@hotmail.com>
2022-09-19 09:50:20 +0100

    listLink created

--- front-sharelink/src/components/ListLinks.js
+++ front-sharelink/src/components/ListLinks.js
@@ -1,10 +1,37 @@
+import { useEffect, useState } from "react";
+import { Navigate } from "react-router-dom";
+import { useUser } from "../context/UserContext";
 import OneLink from "./OneLink";
 
 const ListLinks = () =>{
+
+    const user =useUser()
+   
+    const [data,setData] = useState();
+
+    useEffect(()=>{
+        (async()=>{
+            const res = await fetch('http://127.0.0.1:3000/links',{
+                headers: user ? { 'Athorization' : user.token} :{}
+            })
+            const resData = await res.json();
+            setData(resData)
+          
+
+        })()
+    },[])
+    console.log(data)
+
+    if (!user) {
+        return <Navigate to="/" />
+    }
+
     return(
         <ul>
-
-            <li key='link'> <OneLink link='link'/></li>
+            {data?.data.map(link=>
+                <li key={link.id_link}> <OneLink link={link}/></li>
+                )}
+           
         </ul>
     )
 }
--- front-sharelink/src/components/OneLink.js
+++ front-sharelink/src/components/OneLink.js
@@ -1,11 +1,10 @@
-const OneLink = (  ) =>{
+const OneLink = ({ link }) =>{
     return(
         <section>
-        <h2>title</h2>
-        <a href="url">url</a>
-        <p>description</p>
-        <p>vote</p>
-        <p>author</p>
+        <h2>{link.title}</h2>
+        <a href={link.url}>{link.url}</a>
+        <p>vote: {link.vote}</p>
+        <p>create by {link.email}</p>
         </section>
     )
 
