c36a314821e762626b29a387bc75226c687d7df3
Yaneth Quintero <quinteroftv@hotmail.com>
2022-09-21 19:20:21 +0100

    css changes

--- front-sharelink/src/Pages/Home.js
+++ front-sharelink/src/Pages/Home.js
@@ -1,19 +1,14 @@
 import './Home.css'
 
-const Home = () =>{
-    return(
-        
-        <section className="home-main">
-            <header>
+const Home = () => {
+    return (
+
+        <section className="home">
             <h2>Bienvenid@ a Sharelink</h2>
-            </header>
-            <body>
             <p>
-                Queremos que compartas con nosotros todos aquellos enlaces 
+                Queremos que compartas con nosotros todos aquellos enlaces
                 que te gusten, que quieras dar a conocer a otras personas.
             </p>
-            </body>
-   
         </section>
     )
 }
