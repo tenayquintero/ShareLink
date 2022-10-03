69e5a46909b8599bdf8f51ec7a9c04836e0e9cc8
Yaneth Quintero <quinteroftv@hotmail.com>
2022-10-03 11:50:28 +0100

    editPassword end

--- front-sharelink/src/components/Password/EditPass.js
+++ front-sharelink/src/components/Password/EditPass.js
@@ -1,59 +1,48 @@
-// PUT - '/users/:id/password'
-
 import { useState } from "react";
-//import { useParams } from "react-router-dom";
 import { useUser } from "../../context/UserContext";
-//import useFetch from 'fetch-suspense'
 
 
 const EditPass = () => {
     const user = useUser()
-    //const { id } = useParams();
     const id = user.id;
-    console.log(id);
-   // const newPass = useFetch(`http://127.0.0.1:3000/users/${id}/password`,{
-     //   headers: user ? {'Authorization': user.data} :{}
-       
-    //})
- 
-    
-   const [newpass, setNewPass] = useState("");
-   const [oldpass,setOldpass]=useState("")
+
+    const [newPassword, setNewPass] = useState("");
+    const [oldPassword, setOldpassword] = useState("")
     const [result, setResult] = useState();
-    
-    
+
+
     const handleSubmit = async (e) => {
         e.preventDefault();
         const res = await fetch(`http://127.0.0.1:3000/users/${id}/password`, {
             method: 'PUT',
             headers: user ? { 'Authorization': user.data, "Content-type": "application/json", } : {},
-            body: JSON.stringify({oldpass, newpass})
+            body: JSON.stringify({ oldPassword, newPassword })
 
         });
         const data = await res.json();
-        console.log(data);
+        console.log('soy data de editPassword', data);
         setResult(data);
     }
-   
+
     return (
         <div className="bg">
             <form onSubmit={handleSubmit} className=''>
-            <label> <p>Password Actual</p>
+                <label> <p>Password Actual</p>
                     <input
                         password='password'
-                        value={oldpass}
-                        onChange={(e) => setOldpass(e.target.value)}
+                        value={oldPassword}
+                        onChange={(e) => setOldpassword(e.target.value)}
                     />
                     <p>Nuevo Password</p>
                     <input
                         password='password'
-                        value={newpass}
+                        value={newPassword}
                         onChange={(e) => setNewPass(e.target.value)}
                     />
                 </label>
                 <button>Guardar</button>
             </form>
-           
+
             {result?.status === 'OK' &&
                 <p>Tu Password se ha cambiado correctamente</p>
             }
